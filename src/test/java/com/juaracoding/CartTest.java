6fa2cb24af865afd8a225716e8d3bad88cbb5919
Koswara <koswara.c79@gmail.com>
2024-07-02 20:57:22 +0700

    implement extent test log

--- src/test/java/com/juaracoding/CartTest.java
+++ src/test/java/com/juaracoding/CartTest.java
@@ -4,6 +4,8 @@ import com.juaracoding.drivers.DriverSingleton;
 import com.juaracoding.pages.CartPage;
 import com.juaracoding.pages.HomePage;
 import com.juaracoding.pages.LoginPage;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 import io.cucumber.java.AfterAll;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.BeforeAll;
@@ -16,12 +18,14 @@ import org.testng.Assert;
 
 public class CartTest {
     private WebDriver driver;
+    private ExtentTest extentTest;
     private LoginPage loginPage = new LoginPage();
     private HomePage homePage = new HomePage();
     private CartPage cartPage = new CartPage();
 
     public CartTest(){
         driver = Hooks.driver;
+        extentTest = Hooks.extentTest;
     }
 
     @Given("I am logged in to the application")
@@ -29,31 +33,37 @@ public class CartTest {
         loginPage.clearUsernamePassword();
         loginPage.login("standard_user", "secret_sauce");
         loginPage.clickLoginBtn();
+        extentTest.log(LogStatus.PASS,"I am logged in to the application");
     }
 
     @When("I add a product to the cart")
     public void i_add_a_product_to_the_cart(){
         homePage.setAddBtnToCart();
+        extentTest.log(LogStatus.PASS,"I add a product to the cart");
     }
 
     @Then("I should see total product on icon cart")
     public void i_should_see_total_product_on_icon_cart(){
         Assert.assertEquals(homePage.getTxtCartBadge(), "1");
+        extentTest.log(LogStatus.PASS,"I should see total product on icon cart");
     }
 
     @And("button Add to Cart changed to Remove")
     public void button_add_to_cart_changed_to_remove(){
         Assert.assertEquals(homePage.getTxtRemoveFromCart(), "Remove");
+        extentTest.log(LogStatus.PASS,"button Add to Cart changed to Remove");
     }
 
     @And("I am on the cart page")
     public void i_am_on_the_cart_page(){
         homePage.setCartBtn();
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/cart.html");
+        extentTest.log(LogStatus.PASS,"I am on the cart page");
     }
 
     @And("the product should be in the cart")
     public void the_product_should_be_in_the_cart(){
         Assert.assertEquals(cartPage.getTxtItemName(), "Sauce Labs Backpack");
+        extentTest.log(LogStatus.PASS,"the product should be in the cart");
     }
 }
--- src/test/java/com/juaracoding/LoginTest.java
+++ src/test/java/com/juaracoding/LoginTest.java
@@ -4,6 +4,8 @@ import com.juaracoding.drivers.DriverSingleton;
 import com.juaracoding.pages.HomePage;
 import com.juaracoding.pages.LoginPage;
 import com.juaracoding.utils.Constant;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 import io.cucumber.java.AfterAll;
 import io.cucumber.java.BeforeAll;
 import io.cucumber.java.en.And;
@@ -16,39 +18,45 @@ import org.testng.Assert;
 public class LoginTest {
 
     private WebDriver driver;
+    private ExtentTest extentTest;
 
     private LoginPage loginPage = new LoginPage();
-
     private HomePage homePage = new HomePage();
 
     public LoginTest(){
         driver = Hooks.driver;
+        extentTest = Hooks.extentTest;
     }
 
     @Given("I am on the login page")
     public void i_am_on_the_login_page(){
         driver.get(Constant.URL);
+        extentTest.log(LogStatus.PASS,"I am on the login page");
     }
 
     @When("I enter a valid username and password")
     public void i_enter_a_valid_username_and_password(){
         loginPage.login("standard_user", "secret_sauce");
+        extentTest.log(LogStatus.PASS,"I enter a valid username and password");
     }
 
     @And("I click the login button")
     public void i_click_the_login_button(){
         loginPage.clickLoginBtn();
+        extentTest.log(LogStatus.PASS,"I click the login button");
     }
 
     @Then("I should be redirected to the inventory page")
     public void i_should_be_redirected_to_the_inventory_page(){
         Assert.assertEquals(homePage.getTxtAppLogo(), "Swag Labs");
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html");
+        extentTest.log(LogStatus.PASS,"I should be redirected to the inventory page");
     }
 
     @When("I enter an invalid username or password")
     public void i_enter_an_invalid_username_or_password(){
         loginPage.login("invalid","invalid");
+        extentTest.log(LogStatus.PASS,"I enter an invalid username or password");
     }
 
     @Then("I should see an error message")
@@ -56,6 +64,7 @@ public class LoginTest {
         Assert.assertTrue(loginPage.getTxtError().contains("Username and password do not match"));
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/");
         Assert.assertEquals(loginPage.getAttributeLoginLogo(), "login_log");
+        extentTest.log(LogStatus.PASS,"I should see an error message");
     }
 
 
